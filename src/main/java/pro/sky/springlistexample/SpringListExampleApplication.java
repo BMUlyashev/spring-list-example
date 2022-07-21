aa2d18c35fc16840288cde138168f8b4337f7146
b.ulyashev <b.m.ulyashev@mail.ru>
2022-07-21 18:19:47 +0300

    init

---
+++ src/main/java/pro/sky/springlistexample/SpringListExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springlistexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringListExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringListExampleApplication.class, args);
+    }
+
+}
